f191c651501f9a1ae004142735d545eee9aade5f
ANSHUL-WHY <av4476@srmist.edu.in>
2026-02-16 08:53:33 +0530

    Create .java

---
+++ .java
@@ -0,0 +1,12 @@
+import java.util.*;
+class abc{
+public static void main(String[] args){
+	int a;
+	Scanner sc=new Scanner(System.in);
+System.out.println("enter");
+a=sc.nextInt();
+int b= a*a*a;
+System.out.println("answer"+ b);
+
+}
+}
